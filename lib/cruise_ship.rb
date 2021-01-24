383e873ae96a01c45c682881de7c6a6c6e192082
friendly-format-0074 <samuelagasi@gmail.com>
2021-01-24 18:00:15 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -9,7 +9,7 @@
 def select_winner(passengers)
 winner= ""
   passengers.each do |key, value|
-    if key== suite_a && value.start_with?("A")
+    if key== :suite_a && value.start_with?("A")
     winner== value
   end
 
